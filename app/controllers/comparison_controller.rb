e71dd15d3b2d7f9ab46a9e2ad05b5a768afaa6bc
darrenwong916 <darrenwong06@gmail.com>
2014-02-05 08:50:31 -0800

    WRITING

--- app/controllers/comparison_controller.rb
+++ app/controllers/comparison_controller.rb
@@ -27,10 +27,26 @@ class ComparisonController < ApplicationController
 			@nord = nord_doc.css(".fashion-item")
 		elsif @store_id == '4'
 			sak_url = "http://www.saksfifthavenue.com/search/EndecaSearch.jsp?bmForm=endeca_search_form_one&bmFormID=kf_Qd57&bmUID=kf_Qd58&bmIsForm=true&bmPrevTemplate=%2Fsearch%2FEndecaSearch.jsp&bmText=SearchString&SearchString=#{newsearch}&submit-search=&bmSingle=N_Dim&N_Dim=0&bmHidden=Ntk&Ntk=Entire+Site&bmHidden=Ntx&Ntx=mode%2Bmatchpartialmax&bmHidden=prp8&prp8=t15&bmHidden=prp13&prp13=&bmHidden=sid&sid=143FDD7A465A"
-			binding.pry
+			begin
+				timeout(10) do 
 			sak_doc = Nokogiri::HTML(open(sak_url))
-			binding.pry
 			@sak = sak_doc.css("product-container")
+			binding.pry
+				end
+			rescue Timeout::Error
+			end
+			
+##########################################################
+			# uri = URI.parse(sak_url)
+			# binding.pry
+			# http = Net::HTTP.new(uri.host, uri.port)
+			# http.open_timeout = 10
+			# http.read_timeout = 10
+			# response = http.get(uri.path)
+			# sak = Nokogiri.parse(response.body)
+			# @sak = sak.css('product-container')
+			##########################################################
+			binding.pry
 
 		end
 
